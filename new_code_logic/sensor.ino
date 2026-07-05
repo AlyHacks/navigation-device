68e4442ae92550a364378b4ac6c771c79c6920d3
AlyHacks <137717874+AlyHacks@users.noreply.github.com>
2026-07-05 14:48:45 -0400

    added a new sensor file

--- new_code_logic/sensor.ino
+++ new_code_logic/sensor.ino
@@ -1,47 +1,78 @@
 
 
-
-
 // Include Libraries
 #include <esp_now.h>
 #include <WiFi.h>
-//#include <TimeLib.h>
+#include <Wire.h>
+#include <VL53L1X.h>
+#define SDA_PIN 8
+#define SCL_PIN 9
 
 
-const int ledPin = 4;
+
+
+// Variables for test data
+int int_value;
+float float_value;
+bool bool_value = true;
+VL53L1X sensor;
+
+
+
+
+// MAC Address of responder - edit as required 94:A9:90:6A:7A:58.
+// esp #1:  94:A9:90:67:22:EC
+// esp #2:  94:A9:90:67:03:F8
+
+
+//uint8_t broadcastAddress[] = {0x94, 0xA9, 0x90, 0x6A, 0x7A, 0x58};
+uint8_t broadcastAddress[] = {0x94, 0xA9, 0x90, 0x67, 0x22, 0xEC};  // rcvr #1
+//uint8_t broadcastAddress[] = {0x94, 0xA9, 0x90, 0x67, 0x03, 0xF8};  // rcvr #2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Create a structured object
-float received_distance = 10000;
-
-
-int ontime = 500;
-int distMult = 5;
-
-
-#define bignum 10000000;
-
-
-unsigned long next_pulse = millis() + bignum;
-unsigned long pot_next_pulse = next_pulse;
-unsigned long cur_time;
-unsigned long end_pulse;
 
 
 
 
-// Callback function executed when data is received
-void OnDataRecv(const esp_now_recv_info * mac, const uint8_t *incomingData, int len) {
- memcpy(&received_distance, incomingData, sizeof(received_distance));
- Serial.print("Data received: ");
- Serial.println(len);
- Serial.print("Distance:");
- Serial.println(received_distance);
- Serial.println();
 
 
 
 
+
+
+
+
+// Peer info
+esp_now_peer_info_t peerInfo;
+
+
+
+
+
+
+
+
+// Callback function called when data is sent
+void OnDataSent(const wifi_tx_info_t *mac_addr, esp_now_send_status_t status) {
+Serial.print("\r\nLast Packet Send Status:\t");
+Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
 
@@ -52,52 +83,68 @@ void OnDataRecv(const esp_now_recv_info * mac, const uint8_t *incomingData, int 
 
 
 void setup() {
- pinMode(ledPin, OUTPUT);
-
-
-
-
- // Set up Serial Monitor
+// Set up Serial Monitor
  Serial.begin(115200);
  // Set ESP32 as a Wi-Fi Station
  WiFi.mode(WIFI_STA);
+ Wire.begin(SDA_PIN, SCL_PIN);
+ Wire.setClock(400000); // use 400 kHz I2C
+ sensor.setDistanceMode(VL53L1X::Long);
 
 
 
 
- // Initilize ESP-NOW
+// Initilize ESP-NOW
  if (esp_now_init() != ESP_OK) {
    Serial.println("Error initializing ESP-NOW");
    return;
  }
- // Register callback function
- esp_now_register_recv_cb(OnDataRecv);
 
 
-// single buzz on startup
-   digitalWrite(ledPin, HIGH);
-   delay(ontime);
-   digitalWrite(ledPin,LOW);
 
 
+ // test sensor detection
+ sensor.setTimeout(500);
+ if (!sensor.init()){
+   Serial.println("Failed to detect and initialize sensor!");
+   while (1);
+ }
+
+
+
+
+
+
+
+
+// Register the send callback
+ esp_now_register_send_cb(OnDataSent);
+ // Register peer
+ memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+ peerInfo.channel = 0;
+ peerInfo.encrypt = false;
+ // Add peer     
+ if (esp_now_add_peer(&peerInfo) != ESP_OK){
+   Serial.println("Failed to add peer");
+   return;
+ }
+//set up sensor
+ sensor.setDistanceMode(VL53L1X::Long);
+ sensor.setMeasurementTimingBudget(50000);
+ sensor.startContinuous(50);
+
+
+
+
+ sensor.setROISize(4, 4);    // very narrow (~15°)
+ sensor.setROICenter(199);
 }
 
 
-// At one point in time we start the loop
 
 
 
 
-//            X
-// start   next pulse   end pulse
-//   |        |           |
-// ------------------------------
-
-
-//we take next pulse off the list and make it a huge number after that pulse ends at end pulse
-
-
-// if there is a distance received then the code will predict the potential next pulse, if that potential next pulse is going to happen BEFORE the actual next pulse then it replaces the next pulse. But if the potential next pulse is predicted to be AFTER the next pulse then the pulse will jsut happen at next pulse.
 
 
 
@@ -105,51 +152,39 @@ void setup() {
 
 
 void loop() {
- cur_time = millis();// everymillis we check the time in the loop
-
-
- if(cur_time > next_pulse){ // If the time of the current pulse that is happnening in the moment is passing(interrupting) the time of the next pulse then it turns the buzzer on for that next pulse and also sets end pulse to next pulse plus a new really big variable
-   //start buzz
-   digitalWrite(ledPin, HIGH);
-   end_pulse = next_pulse + ontime;
-   next_pulse = cur_time + bignum;
- }
- if(cur_time > end_pulse){ //Checking when the
-   //end buzz
-   digitalWrite(ledPin, LOW);
-   //digitalWrite(ledPin, LOW);
- }
 
 
 
 
- if (received_distance < 50){
-   //per_end_pulse = cur_time + ontime;
-   pot_next_pulse = ontime + 10 + cur_time; //potential next pulse
- }
- else if (received_distance < 200){
-   pot_next_pulse = distMult*(received_distance/5) + ontime + cur_time;
- }
- else if (received_distance <= 1000 && received_distance >= 200)  {    //led blinks if 10 cm or less away
-   pot_next_pulse = distMult*(received_distance/10) + ontime + cur_time;
- }
- else if (received_distance <= 3000) {
-   pot_next_pulse = distMult*(received_distance/10) + ontime + cur_time;
+ //distance
+ float distance;
+ distance = sensor.read();
+
+
+
+
+
+
+
+
+ // Send message via ESP-NOW
+ esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &distance, sizeof(distance));
+
+
+
+
+ Serial.print(distance);
+ if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+
+
+
+
+ Serial.println();
+ if (result == ESP_OK) {
+   Serial.println("Sending confirmed");
  }
  else {
-   //digitalWrite(ledPin, LOW);
-
-
+   Serial.println("Sending error");
  }
-
-
- if(pot_next_pulse < next_pulse){
-   next_pulse = pot_next_pulse;
- }
- //delayMicroseconds(300);
-
-
-
-
+ delay(200);
 }
-
