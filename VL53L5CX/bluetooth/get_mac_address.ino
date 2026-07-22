fc595c79865581ba31829dde4b189e5de7f73fb9
AlyHacks <137717874+AlyHacks@users.noreply.github.com>
2026-07-22 16:47:32 -0400

    mac address

---
+++ VL53L5CX/bluetooth/get_mac_address.ino
@@ -0,0 +1,16 @@
+#include <WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Set the Wi-Fi mode to Station or AP
+  WiFi.mode(WIFI_STA);
+  
+  // Get and print the MAC address
+  String mac = WiFi.macAddress();
+  Serial.print("ESP32-C3 MAC Address: ");
+  Serial.println(mac);
+}
+
+void loop() {
+}
