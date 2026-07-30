bc49335ffe8f1f9812af5f72c0b83b75d70a6dd7
Calia <caliazychau@gmail.com>
2026-07-29 20:50:24 -0400

    clean up file architecture

---
+++ final/final_buzzer.ino
@@ -0,0 +1,76 @@
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Wire.h>
+#include "esp_wifi.h"
+
+const int buzzerPin = D8; // GPIO pin connected to the buzzer
+//int received_distance = 1;
+int ontime = 200;
+
+// Structure example to receive data
+// Must match the sender structure
+
+/*
+typedef struct struct_message {
+    unsigned char a[32];
+    int d;
+} struct_message;
+*/
+
+int received_minimum;
+
+// Create a struct_message called myData
+//struct_message myData;
+
+//int distance;
+
+// callback function that will be executed when data is received
+void OnDataRecv(const esp_now_recv_info_t *info, const uint8_t *incomingData, int len) {
+  memcpy(&received_minimum, incomingData, sizeof(received_minimum));
+  Serial.println("Distance: ");
+  Serial.print(received_minimum);
+}
+ 
+void setup() {
+
+  pinMode(buzzerPin, OUTPUT);
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+  delay(1000);
+  Serial.print("hello from buzzer esp32");
+  
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  esp_wifi_set_ps(WIFI_PS_NONE);
+  esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
+
+  Wire.begin();
+  Wire.setClock(400000);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  } else {
+    Serial.println("success initializing esp-now");
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+
+  if (distance < 500){
+    digitalWrite(buzzerPin, HIGH);
+    delay(ontime);
+    digitalWrite(buzzerPin, LOW);
+    delay(received_minimum);
+  } else {
+    digitalWrite(buzzerPin, LOW);
+    Serial.print("over 500");
+  }
+
+delay(150);
+}
---
+++ final/final_sensor.ino
@@ -0,0 +1,130 @@
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Wire.h>
+#include "esp_wifi.h"
+#include <SparkFun_VL53L5CX_Library.h>
+#include <iostream>
+#include <algorithm>
+
+#define I2C_SDA D2
+#define I2C_SCL D3
+
+SparkFun_VL53L5CX sensor;
+VL53L5CX_ResultsData results;
+
+int imageResolution = 0; 
+int imageWidth = 0;
+
+// REPLACE with buzzer MAC address: AC:27:6E:7E:A3:A8
+uint8_t broadcastAddress[] = {0xAC, 0x27, 0x6E, 0x7E, 0xA3, 0xA8};
+
+// Structure example to send data
+// Must match the receiver structure
+
+// Create a struct_message called myData
+//struct_message myData;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const wifi_tx_info_t *tx_info, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+  delay(1000);
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  esp_wifi_set_ps(WIFI_PS_NONE);
+  esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
+
+  Wire.begin();
+  Wire.setClock(400000);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 1;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  Serial.println(sensor.getAddress());
+
+  Serial.println("Initializing sensor board. This can take up to 10s. Please wait.");
+  if (sensor.begin() == false)
+  {
+  Serial.println(F("Sensor not found - check your wiring. Freezing"));
+  while (1) ;
+  } else {
+  Serial.println("Sensor has successfully begun.");
+  }
+
+  sensor.setResolution(8*8); //Enable all 64 pads
+  
+  imageResolution = sensor.getResolution(); //Query sensor for current resolution - either 4x4 or 8x8
+  imageWidth = int(sqrt(imageResolution)); //Calculate printing width
+
+  sensor.startRanging();
+  Serial.print("sensor has started ranging.");
+
+
+}
+ 
+void loop() {
+
+  int minimum = 1000000; //fake value to be replaced in the minimum logic
+ 
+  //Poll sensor for new data
+  if (sensor.isDataReady() == true) {
+    if (sensor.getRangingData(&results)) { //Read distance data into array
+        //The ST library returns the data transposed from zone mapping shown in datasheet
+        //Pretty-print data with increasing y, decreasing x to reflect reality
+
+      for (int y = 0 ; y <= imageWidth * (imageWidth - 1) ; y += imageWidth) {
+        for (int x = imageWidth - 1 ; x >= 0 ; x--) {
+          int distance = results.distance_mm[x+y];
+          if(distance < minimum) {
+            minimum = distance;
+          }
+        }
+
+      }
+
+        // Send message via ESP-NOW
+        esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &minimum, sizeof(minimum));
+        
+        if (result == ESP_OK) {
+          Serial.println("Sent with success");
+        }
+        else {
+          Serial.println("Error sending the data");
+        }
+
+      } else {
+        Serial.print("error");
+      }  
+
+
+    }
+    delay(100);
+  }
+
+
