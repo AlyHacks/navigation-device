fca905c3bd3c6e53c5d0191bd61f82a31491a523
Calia <caliazychau@gmail.com>
2026-07-31 17:11:13 -0400

    update sda and scl pins

--- final/final_sensor.ino
+++ final/final_sensor.ino
@@ -6,8 +6,8 @@
 #include <iostream>
 #include <algorithm>
 
-#define I2C_SDA D9
-#define I2C_SCL D10
+#define I2C_SDA D4
+#define I2C_SCL D5
 
 SparkFun_VL53L5CX sensor;
 VL53L5CX_ResultsData results;
