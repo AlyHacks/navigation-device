2cab8381d3956e06870725e63a5018d0a5439b4f
Calia <caliazychau@gmail.com>
2026-08-09 14:33:41 -0400

    added files for sensor methodology

---
+++ sensor_methodology/led.ino
@@ -0,0 +1,49 @@
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Wire.h>
+#include "esp_wifi.h"
+
+const int LED_PIN = D10;
+
+int received_minimum;
+
+void OnDataRecv(const esp_now_recv_info_t *info, const uint8_t *incomingData, int len) {
+  memcpy(&received_minimum, incomingData, sizeof(received_minimum));
+  Serial.println("Distance: ");
+  Serial.print(received_minimum);
+}
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
+  delay(1000);
+  Serial.print("hello from led esp32");
+
+  WiFi.mode(WIFI_STA);
+  esp_wifi_set_ps(WIFI_PS_NONE);
+  esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
+
+  Wire.begin();
+  Wire.setClock(400000);
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  } else {
+    Serial.println("success initializing esp-now");
+  }
+
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+void loop() {
+  if (received_minimum < 1000 || received_minimum == 1000){
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(buzzerPin, LOW);
+    Serial.print("over 1000");
+  }
+
+delay(100);
+
+}
